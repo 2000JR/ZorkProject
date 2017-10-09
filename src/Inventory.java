afe550651f8b97bbe04ab8bba2fecd0c3b6e9b9a
2000JR <32545968+2000JR@users.noreply.github.com>
2017-10-09 10:02:36 -0400

    Add files via upload

---
+++ src/Inventory.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+import java.util.*;
+import java.applet.*;
+import java.awt.*;
+public class Inventory {
+
+}
---
+++ src/Items.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.*;
+import java.applet.*;
+import java.awt.*;
+
+public class Items {
+ 
+
+public class java {
+
+	}
+
+class Item {
+	String name;
+	String ItemDescription;
+	boolean weapon;
+	int quantity;
+	int uses;
+	
+		
+	}
+	
+}
