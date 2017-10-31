0ab1bb1f570e5342cb9c11f32af26072f52bd62b
2000JR <32545968+2000JR@users.noreply.github.com>
2017-10-31 09:55:14 -0400

    Add files via upload

--- src/Game.java
+++ src/Game.java
@@ -46,38 +46,65 @@ public class Game {
 		
 		
 		// Exits
-		Exit e1 = new Exit (Exit.SOUTHWEST, R2F1E1);
-		Exit e2 = new Exit (Exit.NORTHWEST, R1F1E1);
-		Exit e3 = new Exit (Exit.SOUTHWEST, R3F1E1);
-		Exit e4 = new Exit (Exit.SOUTHEAST, R4F1E2);
-		Exit e5 = new Exit (Exit.IN, R5F1E2);
-		Exit e6 = new Exit (Exit.SOUTHEAST, R6F1E2);
+		//Room 1 movement
+		Exit R1e1 = new Exit (Exit.SOUTHWEST, R2F1E1);
+		//room 2 movement
+		Exit R2e2 = new Exit (Exit.NORTHWEST, R1F1E1);
+		Exit R2e3 = new Exit (Exit.SOUTHWEST, R3F1E1);
+		//room 3 movement R3e5 is elevator exit
+		Exit R3e4 = new Exit (Exit.SOUTHEAST, R4F1E2);
+		Exit R3e5 = new Exit (Exit.IN, R5F1E2);
+		Exit R3e6 = new Exit (Exit.SOUTHEAST, R6F1E2);
+		//room 4
+		//Exit e7 = new Exit (Exit.);
 		
-		
-		R1F1E1.addExit ( e1 );
-		R2F1E1.addExit ( e2 );
-		R3F1E1.addExit ( e3 );
-		R4F1E2.addExit ( e4 );
-		R5F1E2.addExit ( e5 );
-		R6F1E2.addExit ( e6 );
+		R1F1E1.addExit ( 	R1e1 );
+		R2F1E1.addExit ( R2e2 );
+		R3F1E1.addExit ( R2e3 );
+		R4F1E2.addExit ( R3e4 );
+		R5F1E2.addExit ( R3e5 );
+		R6F1E2.addExit ( R3e6 );
 		//adding it 
 		Game.addLocation (R1F1E1);
 		Game.addLocation (R2F1E1);
 		Game.addLocation (R3F1E1);
 		Game.addLocation (R4F1E2);
+		Game.addLocation(R5F1E2);
+		Game.addLocation(R6F1E2);
+		Game.addLocation(R7F2E1);
+		Game.addLocation(R8F2E1);
+		Game.addLocation(R9F2E1);
+		Game.addLocation(R9F2E1);
+		Game.addLocation(R10F2E1);
+		Game.addLocation(R11F3E1);
+		Game.addLocation(R12F3E1);
+		Game.addLocation(R12F3E1);
+		Game.addLocation(R13F3E1);
+		Game.addLocation(R14F3E1);
+		Game.addLocation(R15F3E1);
+		Game.addLocation(R16F3E1M0);
+		Game.addLocation(R17F4E1);
+		Game.addLocation(R18F4E1);
+		Game.addLocation(R19F4E1);
+		Game.addLocation(R20F4E1);
 		
-		Game.addExit( e1 );
-		Game.addExit( e2 );
-		Game.addExit( e3 );
-		Game.addExit( e4 );
-		Game.addExit( e5 );
-		Game.addExit( e6 );
+		Game.addExit( R1e1 );
+		Game.addExit( R2e2 );
+		Game.addExit( R2e3 );
+		Game.addExit( R3e4 );
+		Game.addExit( R3e5 );
+		Game.addExit( R3e6 );
 
 		// Set current location
 		Game.setCurrentLocation ( R1F1E1 );
 	
 	
-	
+		showlnglocation();
+	}
+}
+public static void showlnglocation() {
+	System.out.println(currentlocation.getRoomName());
+	System.out.println(currentlocation.getRoomLngDesc());
 	
 	
 }
